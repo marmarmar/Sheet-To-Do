c7c50dcf98dc422679c5e16043469718e61968dd
Stefan Kania <stefankania95@gmail.com>
2017-08-01 13:38:48 +0200

    Init db in defalut controller

---
+++ Sheet-To-Do/Sheet-To-Do/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sheet_To_Do.Models;
+
+namespace Sheet_To_Do.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db = new SheetToDoContext())
+            {
+                var dupa = new Task { Title = "obejrzec kotki" };
+                db.Tasks.Add(dupa);
+                db.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ Sheet-To-Do/Sheet-To-Do/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Sheet_To_Do.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Sheet_To_Do.Models.SheetToDoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Sheet_To_Do.Models.SheetToDoContext";
+        }
+
+        protected override void Seed(Sheet_To_Do.Models.SheetToDoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
