846e530a9b111cc2d9b0a931762d0924e42a2045
Stefan Kania <stefankania95@gmail.com>
2017-09-12 14:45:38 +0200

    Add TaskCategory model

---
+++ Sheet-To-Do/Sheet-To-Do-API/Models/TaskCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sheet_To_Do.Models
+{
+    public class TaskCategory
+    {
+        public int TaskCategoryId { get; set; }
+
+        public int Name { get; set; }
+        public virtual List<Task> Tasks { get; set; }
+
+    }
+}
