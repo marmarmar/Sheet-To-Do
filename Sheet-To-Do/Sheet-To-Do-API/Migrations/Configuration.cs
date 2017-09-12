a3809ad77ecddffe3afee9d842cf805d43933748
Stefan Kania <stefankania95@gmail.com>
2017-09-12 15:39:49 +0200

    Add migrations for TaskCategory

--- Sheet-To-Do/Sheet-To-Do-API/Migrations/Configuration.cs
+++ Sheet-To-Do/Sheet-To-Do-API/Migrations/Configuration.cs
@@ -16,12 +16,18 @@ namespace Sheet_To_Do.Migrations
 
         protected override void Seed(Sheet_To_Do.Models.SheetToDoContext context)
         {
-                context.Tasks.AddOrUpdate(
-                  new Task { Title = "Go to codecool" },
-                  new Task { Title = "Buy some milk" },
-                  new Task { Title = "Go to movie" },
-                  new Task { Title = "Create over app" }
-                );
+            var taskCategory1 = new TaskCategory { Name = "Birthday" };
+            var taskCategory2 = new TaskCategory { Name = "D-Day" };
+            context.TaskCategories.AddOrUpdate(
+                taskCategory1,
+                taskCategory2
+            );
+            context.Tasks.AddOrUpdate(
+                new Task { Title = "Go to codecool" , TaskCategory = taskCategory1},
+                new Task { Title = "Buy some milk" },
+                new Task { Title = "Go to movie" , TaskCategory = taskCategory2},
+                new Task { Title = "Create over app" }
+            );
 
         }
     }
