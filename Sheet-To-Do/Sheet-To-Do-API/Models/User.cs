c3f38a8175d73306e9f755f691a949a2e23e2889
MonikaP <MonikaP@DESKTOP-HP1011I>
2017-09-12 14:25:15 +0200

    Create User model class

---
+++ Sheet-To-Do/Sheet-To-Do-API/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Chronic;
+
+namespace Sheet_To_Do_API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string UserPssword;
+    }
+}
