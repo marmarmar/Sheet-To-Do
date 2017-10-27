f7fe807b4b4333fde1aadd728ace7cb34ec830f3
mar <maristein3@gmail.com>
2017-10-27 21:04:58 +0200

    Remove excessive usings

--- Sheet-To-Do/Sheet-To-Do-API/Controllers/TaskCategoriesController.cs
+++ Sheet-To-Do/Sheet-To-Do-API/Controllers/TaskCategoriesController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-// todo usunąć zbędne referencje
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.Description;
