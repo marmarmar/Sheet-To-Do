cd58209258aa9479132d1387914a580136651ad8
mar <maristein3@gmail.com>
2017-10-27 21:24:49 +0200

    Remove blank lines

--- Sheet-To-Do/Sheet-To-Do-API/Models/User.cs
+++ Sheet-To-Do/Sheet-To-Do-API/Models/User.cs
@@ -6,13 +6,9 @@ namespace Sheet_To_Do_API.Models
     public class User
     {
         public int UserId { get; set; }
-
         [Required(ErrorMessage = "User name is required.")]
         public string Login { get; set; }
-
-        /// <summary>
         ///  TODO - w bazie danych hasło nie powinno być przechowywane otwartym tekstem
-        /// </summary>
         [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
     }
